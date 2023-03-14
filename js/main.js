fc60c185725e02bd5c4427838eb63d9f284bedc0
CarpinchoRecu <123523494+CarpinchoRecu@users.noreply.github.com>
2023-03-14 18:33:55 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let botonCalcular = document.getElementById("btnCalcular");
-botonCalcular.addEventListener("click", function() {
+botonCalcular.addEventListener("click", function () {
 	let campoTotal = document.getElementById("total");
 	let total = campoTotal.value;
-	let porcentaje = 0.1; // porcentaje fijo establecido en el código JavaScript
+	let porcentaje = 0.1;
 	let totalCalculado = total * porcentaje;
 	let columnaTotalCalculado = document.getElementById("totalCalculado");
 	columnaTotalCalculado.textContent = totalCalculado;
